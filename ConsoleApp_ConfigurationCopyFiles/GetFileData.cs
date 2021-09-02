0e336b0dcfb78a23cda62c37e03afec68a5d3657
Danil Khudzhev <hygdeq@gmail.com>
2021-09-02 15:18:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp_ConfigurationCopyFiles/GetFileData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_ConfigurationCopyFiles
+{
+    class GetFileData
+    {
+        public string Source_path { get; private set; }
+        public string Destination_path { get; private set; }
+        public string File_name { get; private set; }
+
+        public GetFileData(string source_path, string destination_path, string file_name)
+        {
+            Source_path = source_path;
+            Destination_path = destination_path;
+            File_name = file_name;
+        }
+
+        public override string ToString()
+        {
+            return Source_path;
+        }
+    }
+}
---
+++ ConsoleApp_ConfigurationCopyFiles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace ConsoleApp_ConfigurationCopyFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"C:\Veeam_Software_C#\ConsoleApp_ConfigurationCopyFiles\ConsoleApp_ConfigurationCopyFiles\ConfigurationFilesCopy.xml");
+            XmlElement xRoot = doc.DocumentElement;
+
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Attributes.Count > 2)
+                {
+                    XmlNode source_path = xnode.Attributes.GetNamedItem("source_path");
+                    XmlNode file_name = xnode.Attributes.GetNamedItem("file_name");
+                    XmlNode destination_path = xnode.Attributes.GetNamedItem("destination_path");
+                    try
+                    {
+                        File.Copy(Path.Combine(source_path.Value.ToString(), file_name.Value.ToString()), Path.Combine(destination_path.Value.ToString(), file_name.Value.ToString()), true);
+                        Console.WriteLine("Файл " + file_name.Value + " перемещен");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Файл " + file_name.Value + " не найден");
+                    }
+                   
+                }
+            }
+        }
+
+    }
+}
